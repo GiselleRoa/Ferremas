2c9fb7954d8dea41e308556295378f1e1fc1f772
GIS ROA <gis.roa@duocuc.cl>
2024-06-04 05:55:09 -0400

    Creacion de clase y controlador de productos y clientes

---
+++ Ferremas.API/Controllers/ProductoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ferremas.API.Models;
+using System.Threading.Tasks;
+
+namespace Ferremas.API.Controllers
+{
+    public class ProductoController : ControllerBase
+    {
+        private static IList<Producto> lista = new List<Producto>();
+
+        //GET
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return lista;
+        }
+        //GET CON ID
+        [HttpGet("{id}")]
+        public Producto get(int id)
+        {
+            return lista.FirstOrDefault(x => x.id == id);
+        }
+        //POST 
+        [HttpPost]
+        public void post([FromBody] Producto value)
+        {
+            lista.Add(value);
+        }
+        // PUT CON ID
+        [HttpPut("{id")]
+        public void Put(int id, [FromBody] Producto value)
+        {
+            Producto selection = lista.FirstOrDefault(x => x.id == id);
+            lista[lista.IndexOf(selection)] = value;
+        }
+
+        //DELETE CON ID
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            lista.Remove(lista.FirstOrDefault(x => x.id == id));
+        }
+
+    }
+}
---
+++ Ferremas.API/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ferremas.API.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public int precio { get; set; }
+        public string nombre { get; set; }
+
+
+    }
+}
